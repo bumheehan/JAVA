3c0c6d5fb7cd84f743fb3300aff5793d0c554d72
Soozl <zlamstn91@gmail.com>
2018-12-28 17:36:50 +0900

    1228

---
+++ JAVA/src/soozl/study/EnhancedFor.java
@@ -0,0 +1,11 @@
+package soozl.study;
+
+public class EnhancedFor {
+
+	public static void main(String[] args) {
+		String[] color = {"red", "yellow", "green"};
+		for(String s : color) {
+			System.out.println(s);
+		}
+	}
+}
