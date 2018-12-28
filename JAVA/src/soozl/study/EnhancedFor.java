01834ef6560c20d6748c9494690720119931c1b4
Soozl <zlamstn91@gmail.com>
2018-12-28 17:42:31 +0900

    soozl

--- JAVA/src/soozl/study/EnhancedFor.java
+++ JAVA/src/soozl/study/EnhancedFor.java
@@ -1,11 +1,21 @@
 package soozl.study;
 
+import java.util.ArrayList;
+
 public class EnhancedFor {
 
 	public static void main(String[] args) {
-		String[] color = {"red", "yellow", "green"};
-		for(String s : color) {
-			System.out.println(s);
+//		String[] color = {"red", "yellow", "green"};
+//		for(String s : color) {
+//			System.out.println(s);
+//		}
+		ArrayList<String> drink = new ArrayList<String>();
+		drink.add("beer");
+		drink.add("coffee");
+		drink.add("coke");
+		drink.add("water");
+		for(String d : drink) {
+			System.out.println(d);
 		}
 	}
 }
